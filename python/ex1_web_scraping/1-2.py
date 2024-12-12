a0e43fbdbf953feb9b424e52d29968fa66c24108
KazutoOhashi <ohashikazuto0505@gmail.com>
2024-12-12 23:52:17 +0900

    add ex2

--- python/ex1_web_scraping/1-2.py
+++ python/ex1_web_scraping/1-2.py
@@ -5,7 +5,6 @@ import pandas as pd
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import WebDriverException, NoSuchElementException
 from selenium.webdriver.chrome.options import Options
 
